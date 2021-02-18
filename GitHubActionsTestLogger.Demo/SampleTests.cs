71d85e8b7a927b57eacb165fd8a530a0acf1b989
Tyrrrz <tyrrrz@gmail.com>
2021-02-19 00:30:09 +0200

    Add a demo project

---
+++ GitHubActionsTestLogger.Demo/SampleTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace GitHubActionsTestLogger.Demo
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void Test1() => Assert.True(true);
+
+        [Fact]
+        public void Test2() => throw new InvalidOperationException();
+
+        [Fact]
+        public void Test3() => Assert.True(false);
+    }
+}
