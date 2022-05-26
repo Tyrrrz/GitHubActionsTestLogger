e57396d4270c9242cc1fcbccac80d43d6b455780
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-05-26 19:34:58 +0300

    Fix relative path generation

--- GitHubActionsTestLogger/Utils/PathEx.cs
+++ GitHubActionsTestLogger/Utils/PathEx.cs
@@ -6,6 +6,16 @@ internal static class PathEx
 {
     // This method exists on .NET5+ but it's impossible to polyfill static
     // members, so we'll just use this one on all targets.
-    public static string GetRelativePath(string basePath, string path) =>
-        Uri.UnescapeDataString(new Uri(new Uri(basePath), path).ToString());
+    public static string GetRelativePath(string basePath, string path)
+    {
+        // Without using System
+        var basePathUri = new Uri(basePath);
+        var pathUri = new Uri(path);
+
+        return Uri.UnescapeDataString(
+            basePathUri
+                .MakeRelativeUri(pathUri)
+                .ToString()
+        );
+    }
 }
